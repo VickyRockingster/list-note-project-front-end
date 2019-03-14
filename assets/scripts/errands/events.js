b63d70f311627d1cba3543ffd84db43ddcaabdea
vickyrockingster <vrockingster@gmail.com>
2019-03-14 16:22:46 -0400

    Set up the styling to create an errand

--- assets/scripts/errands/events.js
+++ assets/scripts/errands/events.js
@@ -4,6 +4,7 @@ const store = require('../store.js')
 
 const onCreateErrand = (event) => {
   event.preventDefault()
+
   api.createErrand()
     .then(ui.createErrandSuccess)
     .catch(ui.failure)
@@ -11,6 +12,7 @@ const onCreateErrand = (event) => {
 
 const onUpdateErrand = (event) => {
   event.preventDefault()
+
   api.updateErrand()
     .then(ui.createErrandSuccess)
     .catch(ui.failure)
@@ -18,6 +20,7 @@ const onUpdateErrand = (event) => {
 
 const onGetErrands = (event) => {
   event.preventDefault()
+
   api.getErrands()
     .then(ui.getErrandsSuccess)
     .catch(ui.failure)
