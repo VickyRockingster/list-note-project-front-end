e8ec6e47767032705bc92c453db645af4c5d8942
vickyrockingster <vrockingster@gmail.com>
2019-04-14 14:28:47 -0400

    Can use .toggle to go between page views (of nav bar) and styled chores same as errands
    -use .toggle instead of .show() and .hide() in app.js
    -chores are now styled in the same way as errands

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -18,7 +18,7 @@ $(() => {
     event.preventDefault()
     choreEvents.onGetChores(event)
     errandEvents.onGetErrands(event)
-    $('#my-lists-button').hide()
+    $('#my-lists-button').toggle()
     $('#account-button').show()
     $('.my-lists-page').show()
     $('.account-page').hide()
@@ -26,7 +26,7 @@ $(() => {
   $('#account-button').on('submit', (event) => {
     event.preventDefault()
     $('#my-lists-button').show()
-    $('#account-button').hide()
+    $('#account-button').toggle()
     $('.my-lists-page').hide()
     $('.account-page').show()
   })
